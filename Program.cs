a18c122be2a4db0b4c0d1a8e46316d91cdb34299
cirobasterra <cirobasterra0@gmail.com>
2026-08-24 09:16:02 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const string NombreComercio = "KIOSCO EL RECREO";
+
+Console.WriteLine($"=== {NombreComercio} ===");
+
+Console.Write("Nombre del cajero: ");
+string nombreCajero = Console.ReadLine();
+
+Console.WriteLine($"Bienvenida, {nombreCajero}. Caja abierta.");
+
+Console.ReadLine();
